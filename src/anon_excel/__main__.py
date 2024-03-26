9ce7c2e7d537dee0e55344aee4ef796a8f6e4aff
Willem Nieuwenhuis <w.nieuwenhuis@utwente.nl>
2024-03-26 17:26:36 +0100

    renamed main function

--- src/anon_excel/__main__.py
+++ src/anon_excel/__main__.py
@@ -1,4 +1,4 @@
-from .transform import main
+from .anon import main
 
 if __name__ == '__main__':
     main()
--- src/anon_excel/anon.py
+++ src/anon_excel/anon.py
@@ -4,6 +4,7 @@ from glob import glob
 from pathlib import Path
 import sys
 import pandas as pd
+from anon_excel.calc_stats import category_to_rank
 
 
 def transform_to_anonymous(df: pd.DataFrame, column: str) -> pd.DataFrame:
@@ -25,7 +26,13 @@ def transform_to_anonymous(df: pd.DataFrame, column: str) -> pd.DataFrame:
 
 def get_parser() -> argparse.ArgumentParser:
     '''Setup a command line parser'''
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(
+        description='''This app will anonymize personal information
+        in user specified columns in an excel table. The personal
+        information is hashed and can be repeated on other excel files
+        producing the same hashed value for the same input.
+        '''
+    )
     parser.add_argument(
         'folder',
         help='Specify the folder with the excel report(s)')
@@ -33,7 +40,17 @@ def get_parser() -> argparse.ArgumentParser:
         '-c', '--column',
         nargs='+',
         required=False,
-        help='Specify a list of fields to process')
+        help='Specify the columns (by name) to make anonymous')
+    parser.add_argument(
+        '-o', '--overwrite',
+        action='store_true',
+        required=False,
+        help='Overwrite existing excel outputs')
+    parser.add_argument(
+        '-r', '--remove',
+        action='store_true',
+        required=False,
+        help='Do not copy personal data to output')
 
     return parser
 
@@ -78,6 +95,7 @@ def main():
 
         df = read_and_clean(name, col[0])
         df = transform_to_anonymous(df, col[0])
+        df = category_to_rank(df)
         df.to_excel(outname, index=False)
 
 
