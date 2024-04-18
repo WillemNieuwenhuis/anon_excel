4f5f9c278f3bb7b28430ba8c260de7f75f004ba3
Willem Nieuwenhuis <w.nieuwenhuis@utwente.nl>
2024-04-18 15:16:51 +0200

    test on anonimizing names

--- tests/test_transform.py
+++ tests/test_transform.py
@@ -1,5 +1,31 @@
-import pytest
+import pandas as pd
+
+from anon_excel.anon import transform_to_anonymous
+
+ANONIMIZE_DATA = [{'Your student number': 's12345',
+                   'q1': 'Agree (A)',
+                   'q2': 'Disagree (D)'},
+                  {'Your student number': 's125676',
+                   'q1': 'Agree (A)',
+                   'q2': 'Agree (A)'}]
 
 
-def test_transform_load():
-    assert False
+def test_transform_to_anonymous_no_id_col():
+    '''test no change when on_column is missing in df'''
+    df = pd.DataFrame(ANONIMIZE_DATA)
+
+    df_new = transform_to_anonymous(df, 'My student number', 'anon')
+
+    assert df_new.equals(df)
+
+
+def test_transform_to_anonymous_id_col_removed():
+    '''test that the on column data is removed after
+       succesfully anonimizing student numbers and the new
+       anonimzed column has been added
+    '''
+    df = pd.DataFrame(ANONIMIZE_DATA)
+
+    df_new = transform_to_anonymous(df, 'Your student number', 'anon')
+    assert 'Your student number' not in df_new.columns
+    assert 'anon' in df_new.columns
