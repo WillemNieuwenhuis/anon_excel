2701fee824c376a01af8381e5d54a5a59c020899
Willem Nieuwenhuis <w.nieuwenhuis@utwente.nl>
2024-04-19 11:52:19 +0200

    Added test checking proper removal

--- tests/test_survey_files.py
+++ tests/test_survey_files.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 import mock
 
-from anon_excel.anon import find_survey_files
+from anon_excel.anon import find_survey_files, remove_previous_results
 
 
 @mock.patch('anon_excel.anon.glob')
@@ -33,3 +33,11 @@ def test_find_survey_files_two_of_two(mock_exists, mock_glob):
     assert len(files) == 2
     assert files[0][1] == Path('Post_survey1.xlsx')
     assert files[1][1] == Path('Post_survey2.xlsx')
+
+
+@mock.patch('anon_excel.anon.os.remove')
+def test_remove_previous_results_called(mock_remove):
+    files = [Path(p) for p in ['analysis_course1.xlsx', 'analysis_course2.xlsx']]
+    remove_previous_results(files)
+    assert mock_remove.call_count == 2
+    assert mock_remove.call_args_list == [mock.call(f) for f in files]
